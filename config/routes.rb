0a47ef2ae10a5f3d1b30fe07d0ddc678f8c1b26d
Fernando Paredes <nano@fdp.io>
2013-09-10 10:09:20 -0700

    Add Books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Boxylib::Application.routes.draw do
+  get "books/index"
+  get "books/show"
+  get "books/new"
+  get "books/edit"
+  get "books/update"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
