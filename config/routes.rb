708745c96c4e6f21a8e2c2634361fe2d59d556c7
Fernando Paredes <nano@fdp.io>
2013-09-13 09:37:12 -0700

    Generate Static controller for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Boxylib::Application.routes.draw do
+  root "sites#index"
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
