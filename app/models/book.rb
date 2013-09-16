2eb14e02ff1cbff6e65dc971eeffe281997fa19e
Fernando Paredes <nano@fdp.io>
2013-09-15 18:23:11 -0700

    Add reference to User in Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
+  belongs_to :user
   validates :isbn, length: { maximum: 13 }, presence: true
   validates :isbn, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913205916) do
+ActiveRecord::Schema.define(version: 20130916011327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,18 @@ ActiveRecord::Schema.define(version: 20130913205916) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id", using: :btree
+
+  create_table "libraries", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "libraries", ["user_id"], name: "index_libraries_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
