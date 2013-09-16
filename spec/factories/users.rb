2b5b5711df215b013cfeb86b817f62b1a61ff031
Fernando Paredes <nano@fdp.io>
2013-09-15 18:06:22 -0700

    Add factory and tests for User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    password_confirmation { |u| u.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.build(:user) }
+
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+
+  it 'is invalid without a password' do
+    user.password = ""
+    expect(user).not_to be_valid
+  end
+
+  it 'is invalid with a wrong password confirmation' do
+    user.password_confirmation = "lk"
+    expect(user).to have(1).errors_on(:password_confirmation)
+  end
+
+  it 'is invalid without an email address' do
+    user.email = nil
+    expect(user).to have(1).errors_on(:email)
+  end
 end
