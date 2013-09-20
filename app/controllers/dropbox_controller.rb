bc83fffd972468bee89d317870ff674a86e57781
Fernando Paredes <nano@fdp.io>
2013-09-20 16:56:18 -0700

    Fix spacing on dropbox controller

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -24,99 +24,99 @@ AUTHORIZED_UPLOADS = /\.(pdf|mobi|epub)$/
 
 class DropboxController < ApplicationController
 
-    def main
-        client = get_dropbox_client
-        unless client
-            redirect_to(:action => 'auth_start') and return
-        end
-
-        account_info = client.account_info
-
-        # Show a file upload page
-        render 'upload'
+  def main
+    client = get_dropbox_client
+    unless client
+      redirect_to(:action => 'auth_start') and return
     end
 
-    def upload
-        client = get_dropbox_client
-        unless client
-            redirect_to(:action => 'auth_start') and return
-        end
+    account_info = client.account_info
 
-        begin
-            # Upload the POST'd file to Dropbox, creates a folder with a name of Author/Author - Title.ext
-            epub = EPUBInfo.get(params[:file].tempfile)
-            author = epub.creators.first.name
-            title = epub.titles.first
-            folder = "#{author}/#{title}/"
-            isbn = epub.identifiers.first.identifier.gsub(/\D/, '')
+    # Show a file upload page
+    render 'upload'
+  end
 
-            full_path = folder + "#{author} - #{title}.epub"
-            resp = client.put_file(full_path, params[:file].read)
-            cover = client.put_file("#{folder}cover.jpg", epub.cover.tempfile)
-            book = Book.create(user_id: current_user.id, title: title, author: author, isbn: isbn)
-            render :text => "Upload successful.  File now at #{resp['path']}"
-        rescue DropboxAuthError => e
-            session.delete(:access_token)  # An auth error means the access token is probably bad
-            logger.info "Dropbox auth error: #{e}"
-            render :text => "Dropbox auth error"
-        rescue DropboxError => e
-            logger.info "Dropbox API error: #{e}"
-            render :text => "Dropbox API error"
-        end
+  def upload
+    client = get_dropbox_client
+    unless client
+      redirect_to(:action => 'auth_start') and return
     end
 
-    def parse
-      # Need to figure out how to get uploaded file poss. params[:file]
-      parser = EPUBInfo.get(ebook)
-    end
+    begin
+      # Upload the POST'd file to Dropbox, creates a folder with a name of Author/Author - Title.ext
+      epub = EPUBInfo.get(params[:file].tempfile)
+      author = epub.creators.first.name
+      title = epub.titles.first
+      folder = "#{author}/#{title}/"
+      isbn = epub.identifiers.first.identifier.gsub(/\D/, '')
 
-    def get_dropbox_client
-        if session[:access_token]
-            begin
-                access_token = session[:access_token]
-                DropboxClient.new(access_token)
-            rescue
-                # Maybe something's wrong with the access token?
-                session.delete(:access_token)
-                raise
-            end
-        end
+      full_path = folder + "#{author} - #{title}.epub"
+      resp = client.put_file(full_path, params[:file].read)
+      cover = client.put_file("#{folder}cover.jpg", epub.cover.tempfile)
+      book = Book.create(user_id: current_user.id, title: title, author: author, isbn: isbn)
+      render :text => "Upload successful.  File now at #{resp['path']}"
+    rescue DropboxAuthError => e
+      session.delete(:access_token)  # An auth error means the access token is probably bad
+      logger.info "Dropbox auth error: #{e}"
+      render :text => "Dropbox auth error"
+    rescue DropboxError => e
+      logger.info "Dropbox API error: #{e}"
+      render :text => "Dropbox API error"
     end
+  end
 
-    def get_web_auth()
-        redirect_uri = url_for(:action => 'auth_finish')
-        DropboxOAuth2Flow.new(APP_KEY, APP_SECRET, redirect_uri, session, :dropbox_auth_csrf_token)
+  def parse
+    # Need to figure out how to get uploaded file poss. params[:file]
+    parser = EPUBInfo.get(ebook)
+  end
+
+  def get_dropbox_client
+    if session[:access_token]
+      begin
+        access_token = session[:access_token]
+        DropboxClient.new(access_token)
+      rescue
+        # Maybe something's wrong with the access token?
+        session.delete(:access_token)
+        raise
+      end
     end
+  end
 
-    def auth_start
-        authorize_url = get_web_auth().start()
+  def get_web_auth()
+    redirect_uri = url_for(:action => 'auth_finish')
+    DropboxOAuth2Flow.new(APP_KEY, APP_SECRET, redirect_uri, session, :dropbox_auth_csrf_token)
+  end
 
-        # Send the user to the Dropbox website so they can authorize our app.  After the user
-        # authorizes our app, Dropbox will redirect them here with a 'code' parameter.
-        redirect_to authorize_url
+  def auth_start
+    authorize_url = get_web_auth().start()
+
+    # Send the user to the Dropbox website so they can authorize our app.  After the user
+    # authorizes our app, Dropbox will redirect them here with a 'code' parameter.
+    redirect_to authorize_url
+  end
+
+  def auth_finish
+    begin
+      access_token, user_id, url_state = get_web_auth.finish(params)
+      session[:access_token] = access_token
+      redirect_to :action => 'main'
+    rescue DropboxOAuth2Flow::BadRequestError => e
+      render :text => "Error in OAuth 2 flow: Bad request: #{e}"
+    rescue DropboxOAuth2Flow::BadStateError => e
+      logger.info("Error in OAuth 2 flow: No CSRF token in session: #{e}")
+      redirect_to(:action => 'auth_start')
+    rescue DropboxOAuth2Flow::CsrfError => e
+      logger.info("Error in OAuth 2 flow: CSRF mismatch: #{e}")
+      render :text => "CSRF error"
+    rescue DropboxOAuth2Flow::NotApprovedError => e
+      render :text => "Not approved?  Why not, bro?"
+    rescue DropboxOAuth2Flow::ProviderError => e
+      logger.info "Error in OAuth 2 flow: Error redirect from Dropbox: #{e}"
+      render :text => "Strange error."
+    rescue DropboxError => e
+      logger.info "Error getting OAuth 2 access token: #{e}"
+      render :text => "Error communicating with Dropbox servers."
     end
-
-    def auth_finish
-        begin
-            access_token, user_id, url_state = get_web_auth.finish(params)
-            session[:access_token] = access_token
-            redirect_to :action => 'main'
-        rescue DropboxOAuth2Flow::BadRequestError => e
-            render :text => "Error in OAuth 2 flow: Bad request: #{e}"
-        rescue DropboxOAuth2Flow::BadStateError => e
-            logger.info("Error in OAuth 2 flow: No CSRF token in session: #{e}")
-            redirect_to(:action => 'auth_start')
-        rescue DropboxOAuth2Flow::CsrfError => e
-            logger.info("Error in OAuth 2 flow: CSRF mismatch: #{e}")
-            render :text => "CSRF error"
-        rescue DropboxOAuth2Flow::NotApprovedError => e
-            render :text => "Not approved?  Why not, bro?"
-        rescue DropboxOAuth2Flow::ProviderError => e
-            logger.info "Error in OAuth 2 flow: Error redirect from Dropbox: #{e}"
-            render :text => "Strange error."
-        rescue DropboxError => e
-            logger.info "Error getting OAuth 2 access token: #{e}"
-            render :text => "Error communicating with Dropbox servers."
-        end
-    end
+  end
 end
